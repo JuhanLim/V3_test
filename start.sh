6e2decb87a1b7bb421a5282f3751f0a2ba7658ee
topazlas <yimkyeongbin@gmail.com>
2024-06-27 16:31:21 +0900

    first commit

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ echo "==> 패키지 다운로드"
 pip install -r /code/requirements.txt
 
 echo "==> 배포!"
-gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=test_page.settings test_page.wsgi:application
+gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=V3.settings V3.wsgi:application
